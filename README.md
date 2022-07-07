a19904dac5893260ba2367772330805f68530137
Ever Argollo <everfavioat@gmail.com>
2022-07-07 13:24:26 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+### POC socket io
+
---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const { createClient } = require('redis');
+const { createAdapter } = require('@socket.io/redis-adapter');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.io = io;
+  next();
+});
+
+
+app.post('/notifications', (req, res) => {
+  console.log(req.body.message);
+  notification = req.body.message;
+  io.sockets.emit('chat message', notification);
+  res.send('ok');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+    io.on('connection', (socket) => {
+      console.log('a user connected');
+      socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
+    });
+  });
+  
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
